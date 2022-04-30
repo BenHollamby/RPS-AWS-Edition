84e9a2f8b2211c5f0a667b568bf48a528899b33b
Ben Hollamby <ben.hollamby@outlook.com>
2022-04-30 22:58:27 +1200

    create multi az rds instance with secret and puts out endpoint address

--- createrds.py
+++ createrds.py
@@ -1,24 +1,36 @@
 import boto3
 import json
 
-client = boto3.client('secretsmanager')
+secret = boto3.client('secretsmanager')
 
-response = client.get_secret_value(
+response = secret.get_secret_value(
     SecretId='RPS-DB-Creds'
 )
 
 database_secrets = json.loads(response['SecretString'])
-#print(database_secrets['RPS-MySQL-DB-Creds'])
+rds = boto3.client('rds')
 
-client = boto3.client('rds')
-
-response = client.create_db_instance(
+response = rds.create_db_instance(
     AllocatedStorage=5,
     DBInstanceClass='db.t2.micro',
-    DBInstanceIdentifier='RPS-database-MySQL-01',
+    DBInstanceIdentifier='RPS-database-MySQL-02',
     Engine='MySQL',
     MasterUserPassword=database_secrets['RPS-MySQL-DB-Creds'],
     MasterUsername='admin',
+    MultiAZ=True
 )
 
-print(response)
+DBInstanceIdentifier='RPS-database-MySQL-02'
+
+response = rds.describe_db_instances(DBInstanceIdentifier=DBInstanceIdentifier)
+
+while True:
+    response = rds.describe_db_instances(DBInstanceIdentifier=DBInstanceIdentifier)
+    status = response['DBInstances'][0]['DBInstanceStatus']
+    if status != 'available':
+        continue
+    else:
+        break
+
+address = response.get('DBInstances')[0].get('Endpoint').get('Address')
+print(address)
